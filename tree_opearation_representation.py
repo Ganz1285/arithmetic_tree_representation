dcd5121184c70198d0c5c9ca117a4d134e6bca8f
Ganz1285 <selvaganz1285@gmail.com>
2023-08-24 16:16:48 +0530

    implemented parsing through linked list

--- tree_opearation_representation.py
+++ tree_opearation_representation.py
@@ -1,78 +1,72 @@
 import re
 
+high_precedence=[]
+low_precedence=[]
 
 class operator:
     def __init__(self,op,l,r):
         self.op=op
         self.l=l
         self.r=r
-        self.result=int(eval(self.l+self.op+self.r))
+        self.result=None
+        self.left=None
+        self.right=None
+
 
     def representation(self):
         print(str(self.l)+" "*(len(str(self.result)))+str(self.r))
         print(" "*(len(str(self.l))-1)+"\\"+self.op.center((len(str(self.result)))," ")+"/")
         print(" "*len(str(self.l))+str(self.result))
  
-def extracter(s,ind):
-    z=""
-    for i in range(ind-1,-1,-1):
-        if s[i].isdigit():
-            z+=s[i]
-        else:
-            break
-    z=z[::-1]
-    p=""
-    for i in range(ind+1,len(s)):
-        if s[i].isdigit():
-            p+=s[i]
-        else:
-            break
-    return (z,p) 
     
 
 
 
 def splitter(s):
-    l=[]
-    while '*' in s:
-        x=s.index('*')
-        l.append((('*'),extracter(s,x)))
-        s=s[:x]+'~'+s[x+1:]
-    while '/' in s:
-        x=s.index('/')
-        l.append((('/'),extracter(s,x)))
-        s=s[:x]+'~'+s[x+1:]
-    while '+' in s:
-        x=s.index('+')
-        l.append((('+'),extracter(s,x)))
-        s=s[:x]+'~'+s[x+1:]
-    while '-' in s:
-        x=s.index('-')
-        l.append((('-'),extracter(s,x)))
-        s=s[:x]+'~'+s[x+1:]
- 
-    return l
-
-
-
-
+    ext=""
+    recent=None
+    for i in range(len(s)):
+        if s[i].isdigit():
+            ext+=s[i]
+        else:
+            op=s[i]
+            l=ext
+            r=""
+            for j in range(i+1,len(s)):
+                if s[j].isdigit():r+=s[j]
+                else:break
+            root=operator(op,l,r)
+            if not recent:
+                recent=root
+            else:
+                root.left=recent
+                recent.right=root
+                recent=recent.right
+            
+            if op in "*/":high_precedence.append(root)
+            else:low_precedence.append(root)
+            ext=""
 
 def valid_operation(s):
     """
-    Split input by operators
     check if first and last elements are operands. not operators
     """
     pattern=r"^[0-9][0-9+\-*/]*[0-9]$"
-    if re.match(pattern,s):
-        cond=True
-        for i in splitter(s):
-            if i[1][0].isdigit() and i[1][1].isdigit():
-                pass
-            else:
-                cond=False
-    else:
-        cond=False
-    return cond
+    return re.match(pattern,s)
+
+def parse():
+
+    root=high_precedence[0] if high_precedence!=[] else low_precedence[0]
+    pend=None
+    if root.left:
+        pend=root.left
+    while root:
+        print(root.l,root.op,root.r)
+        root=root.right
+    while pend.left:pend=pend.left    
+    while pend:
+        print(pend.l,pend.op,pend.r)
+        pend=pend.right
 
 
 
@@ -82,11 +76,11 @@ def main():
         print("VALID")
     else:
         print("INVALID")
-    l=[]
-    for i in splitter(s):
-        l.append(operator(i[0],i[1][0],i[1][1]))
-    for i in l:
-        i.representation()
+
+    splitter(s)
+    parse()
+
+
 
 if __name__=="__main__":
     main()
